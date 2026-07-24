05499d3a64df75529d0a8b763237762cf18fabf6
pluck1983-source <pluck1983@gmail.com>
2026-07-24 10:50:14 +0100

    Add files via upload

---
+++ childminder-pocket-informant/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pluck1983.childminderincometracker',
+  appName: 'Childminder Tracker',
+  webDir: 'dist'
+};
+
+export default config;
