86993c15e492b3e496d580aa7b2f49f93d51027d
pluck1983-source <pluck1983@gmail.com>
2026-07-24 10:56:54 +0100

    Delete childminder-pocket-informant directory

--- childminder-pocket-informant/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.pluck1983.childminderincometracker',
-  appName: 'Childminder Tracker',
-  webDir: 'dist'
-};
-
-export default config;
